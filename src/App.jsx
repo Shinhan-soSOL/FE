93f6f4f70ec87ebd7c556dd4aab12fbb2a80f8f8
KimRiun <kimriun27@gmail.com>
2024-10-23 15:17:38 +0900

    feat: react-router-dom 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import './App.css';
-import Landing from './pages/Landing';
-import theme from './theme.js';
-import { ThemeProvider } from '@mui/material';
 
 function App() {
-  return (
-    <div className='w-full h-full font-shinhan'>
-      <ThemeProvider theme={theme}>
-        <Landing />
-      </ThemeProvider>
-    </div>
-  );
+  return <div className='w-full h-full font-shinhan'></div>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
+import theme from './theme.js';
+import Landing from './pages/Landing.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </ThemeProvider>
   </StrictMode>
 );
