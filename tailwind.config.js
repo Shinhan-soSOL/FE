d552900d61a544a46956aac843323e3e206798aa
KimRiun <kimriun27@gmail.com>
2024-10-23 13:58:41 +0900

    init: mui와 tailwind 활용한 color 커스터마이징

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      's-blue': {
+        50: '#F4F9FF',
+        100: '#CCDAFF',
+        200: '#99B5FE',
+        300: '#6690FE',
+        400: '#336BFD',
+        500: '#0046FD',
+        600: '#0038CA',
+        700: '#002A98',
+        800: '#001C65',
+        900: '#000E33',
+      },
+      'ch-red': '#FF5B5B',
+      'ch-blue': '#5B9DFF'
+    },
     fontFamily: {
       'shinhan': ["OneShinhanMedium", "sans"],
       'shinhan-b': ["OneShinhanBold"],
