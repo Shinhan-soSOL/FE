f165ed06ce9a7766237c8673b5b47fcbefc90fc8
KimRiun <kimriun27@gmail.com>
2024-10-24 16:05:50 +0900

    feat: 홈의 타이틀에 버튼 들어간 경우 고려해 수정

--- src/components/home/Title.jsx
+++ src/components/home/Title.jsx
@@ -1,22 +1,20 @@
+import { IoIosArrowForward } from 'react-icons/io';
+
 export default function Title({ text, onClick }) {
   return (
     <div className='flex justify-between items-center'>
       <div className='flex items-center gap-2'>
-        <div className='text-lg font-shinhan-b'>{text}</div>
-        {onClick && (
-          <div
-            onClick={() => {
-              if (onClick) {
-                console.log('버튼 제목');
-                onClick();
-              }
-            }}
-          >
-            자세히보기
-          </div>
-        )}
+        <div
+          className='text-lg font-shinhan-b'
+          onClick={() => {
+            if (onClick) onClick();
+          }}
+        >
+          {text}
+        </div>
+        {onClick && <IoIosArrowForward size={20} className=' text-s-gray-300' />}
       </div>
-      <div className=' font-sm text-s-gray-300'>수정</div>
+      {!onClick && <div className=' font-sm text-s-gray-300'>수정</div>}
     </div>
   );
 }
