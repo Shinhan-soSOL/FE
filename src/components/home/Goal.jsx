37fda071a6ba49b0e21183e4b80de1ae905890d9
KimRiun <kimriun27@gmail.com>
2024-10-24 14:28:16 +0900

    feat: 홈의 목표 종목 컴포넌트 구현

---
+++ src/components/home/Goal.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import Title from './Title';
+
+export default function Goal({ data }) {
+  const { stockName, currentPrice } = data;
+
+  return (
+    <div className='w-full p-4 px-5 bg-white'>
+      <Title text='목표' />
+      <div className='p-4'>
+        <p className=' font-bold text-xl text-s-gray-400'>{stockName}</p>
+        <p className=' font-extrabold text-2xl'>{currentPrice}원</p>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,35 @@
+import { useState } from 'react';
+import Goal from '../components/home/Goal';
+
 export default function Home() {
-  return <div>Home</div>;
+  const [data, setData] = useState({
+    changeSum: 990,
+    changeAmount: 30,
+    investedAmount: 3400,
+    totalValue: 1333,
+    profitRatio: 12,
+    goal: {
+      stockName: '삼성전자',
+      stockCode: '111111',
+      currentPrice: 45000,
+    },
+    myStocks: [
+      {
+        stockName: '삼성전자',
+        stockCode: '111111',
+        quantity: 2,
+        investedAmount: 87000,
+        profit: 13,
+        total: 897000,
+        avgPrice: 46000,
+        profitRatio: 15.4,
+        currentPrice: 56000,
+      },
+    ],
+  });
+  return (
+    <div className='w-full h-full bg-s-blue-50 flex flex-col gap-2'>
+      <Goal data={...data.goal} />
+    </div>
+  );
 }
